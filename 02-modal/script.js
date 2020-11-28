ab4226ed4749c85e13a9bba786c9ac21aaa0b2ab
boghdady <boghdady107@gmail.com>
2020-11-28 22:05:23 +0200

    Finish modal logic

--- 02-modal/script.js
+++ 02-modal/script.js
@@ -5,7 +5,30 @@ const btnCloseModal = document.querySelector('.close-modal');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 
-
-for (let i = 0; i < btnShowModal.length; i++) {
-  console.log(btnShowModal[i].textContent)
+const openModal = function () {
+  modal.classList.remove(['hidden']);
+  overlay.classList.remove(['hidden']);
 }
+
+const closeModal = function () {
+  modal.classList.add(['hidden']);
+  overlay.classList.add(['hidden']);
+}
+
+// Show modal
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener('click', openModal);
+}
+
+// 1) Close modal when click close botton
+btnCloseModal.addEventListener('click', closeModal);
+
+// 2) Close modal when click on overlay
+overlay.addEventListener('click', closeModal);
+
+// 3) Close modal when click "Esc" key
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+})
