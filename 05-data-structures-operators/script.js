7291ae3a6d7eb63f4a2cde8d3fc7b34017d392a7
boghdady <boghdady107@gmail.com>
2020-12-12 16:44:04 +0200

    Strings methods (indexOf()- lastIndexOf()- slice()- trim()- toLoweCase())

--- 05-data-structures-operators/script.js
+++ 05-data-structures-operators/script.js
@@ -8,14 +8,10 @@ const restaurant = {
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
   order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
   // es6 enhanced write function inside object literal
-  orderDelivery({
-    time,
-    starterIndex,
-    mainIndex = 2
-  }) {
+  orderDelivery({ time, starterIndex, mainIndex = 2 }) {
     console.log(time, this.starterMenu[starterIndex], this.mainMenu[mainIndex]);
   },
 
@@ -36,7 +32,7 @@ const restaurant = {
 };
 
 // ------------------Destructuring Arrays-------------------------//
-console.log("---------Destructuring Arrays------------");
+console.log('---------Destructuring Arrays------------');
 // 1) Destructuring
 let [itailia, pizzeria, , organic] = restaurant.categories;
 console.log(itailia, pizzeria, organic);
@@ -48,7 +44,7 @@ console.log(first, second);
 console.log(first, second);
 
 // 3) Nested Array
-const arr = [1, 2, ["a", "b"]];
+const arr = [1, 2, ['a', 'b']];
 const [num1, , [a, b]] = arr;
 console.log(num1, a, b);
 
@@ -62,13 +58,9 @@ const [starterMenueItem, mainMenueItem] = restaurant.order(2, 1);
 console.log(starterMenueItem, mainMenueItem);
 
 // ------------------Destructuring Objects-------------------------//
-console.log("---------Destructuring Objects------------");
+console.log('---------Destructuring Objects------------');
 // 1) Normal destructuring
-const {
-  name,
-  categories,
-  location: restaurantLocation
-} = restaurant;
+const { name, categories, location: restaurantLocation } = restaurant;
 
 console.log(name, categories, restaurantLocation);
 
@@ -76,7 +68,7 @@ console.log(name, categories, restaurantLocation);
 const {
   name: restaurantName,
   categories: restaurantVCategories,
-  menu = []
+  menu = [],
 } = restaurant;
 
 console.log(restaurantName, restaurantVCategories, menu);
@@ -87,21 +79,15 @@ let b1 = 20;
 
 const obj = {
   a1: 4,
-  b1: 5
+  b1: 5,
 };
 
-({
-  a1,
-  b1
-} = obj)
+({ a1, b1 } = obj);
 console.log(a1, b1);
 
 // 4) Nested Object
 const {
-  fri: {
-    open,
-    close
-  }
+  fri: { open, close },
 } = restaurant.openingHours;
 console.log(open, close);
 
@@ -114,14 +100,14 @@ restaurant.orderDelivery({
 
 // ------------------Spread Operators-------------------------//
 // Goal: 1) build new array , 2) path multiple variables to function params
-console.log("---------Spread Operators------------");
+console.log('---------Spread Operators------------');
 
 // 1) Join 2 arrays or more
 const fullMenu = [...restaurant.mainMenu, ...restaurant.starterMenu];
 console.log(fullMenu);
 
 // 2) Copy array
-const copyMenu = ["checken", ...fullMenu];
+const copyMenu = ['checken', ...fullMenu];
 console.log(copyMenu);
 
 // 3) Function args
@@ -134,26 +120,22 @@ sum(...numbers);
 
 // Copy object (shallow coping)
 const copyRestuarant = {
-  ...restaurant
+  ...restaurant,
 };
 console.log(copyRestuarant);
 
-
 // ------------------Rest Operators-------------------------//
-console.log("---------Rest Operators------------");
+console.log('---------Rest Operators------------');
 
 // SPREAD: because of on right of "="
 const arrSp = [1, 2, ...[3, 4]];
 // REST: because of on right of "="
 const [firstEl, secondEl, ...others] = [20, 30, 40, 50];
-console.log(firstEl, secondEl, others)
+console.log(firstEl, secondEl, others);
 
 // 2) objects
 
-const {
-  fri,
-  ...otherDays
-} = restaurant.openingHours;
+const { fri, ...otherDays } = restaurant.openingHours;
 console.log(fri, otherDays);
 
 // 3) funcation
@@ -174,19 +156,16 @@ const numsArr = [2, 4, 2, 5, 2, 56, 2];
 add(...numsArr);
 
 // ------------------Rest Operators-------------------------//
-console.log("---------Circuiting------------");
+console.log('---------Circuiting------------');
 
 // 1) || Operator
 // Will return the first trully value if all is false will return the last false value
-console.log("---------OR------------");
+console.log('---------OR------------');
 
-console.log(undefined || "" || 3);
-console.log("" || null || false || 0 || undefined);
+console.log(undefined || '' || 3);
+console.log('' || null || false || 0 || undefined);
 
-
-
-
-console.log("---------Nullish Operator------------");
+console.log('---------Nullish Operator------------');
 // Special case (|| not good solution here)
 restaurant.numGuests = 0;
 const guests = restaurant.numGuests || 10;
@@ -197,73 +176,75 @@ console.log(guests);
 const correctGuests = restaurant.numGuests ?? 10;
 console.log(correctGuests);
 
-// 2) && Operators 
+// 2) && Operators
 // Will return the first false value, if all values are false will return the last false value
-console.log("---------&&------------");
+console.log('---------&&------------');
 console.log(3 && true && null && 4);
 console.log(3 && true && 5);
 
 // ------------------Arrays loops-------------------------//
-console.log("---------Arrays loops------------");
+console.log('---------Arrays loops------------');
 // 1) for of loop
 const menuArr = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
 for (const item of menuArr) console.log(item);
 
 // Print index and value
-for (const [index, value] of menuArr.entries()) console.log(`${index +1} : ${value}`);
+for (const [index, value] of menuArr.entries())
+  console.log(`${index + 1} : ${value}`);
 
 // ------------------Chaining operator ".?"-----------------------//
 
-console.log("--Mix chainning operator with Nullish operator--");
+console.log('--Mix chainning operator with Nullish operator--');
 // "?." : if channing poroperty is not exist will retrun undefinded
-const openHour = restaurant.openingHours?.open ?? "close";
+const openHour = restaurant.openingHours?.open ?? 'close';
 
 console.log(openHour);
 
 // With arrays
-const usersArr = [{
-    name: "Ahmed",
-    email: "ahmed@example.com"
+const usersArr = [
+  {
+    name: 'Ahmed',
+    email: 'ahmed@example.com',
   },
   {
-    name: "mohamed",
-    email: "mohamed@example.com"
-  }
+    name: 'mohamed',
+    email: 'mohamed@example.com',
+  },
 ];
 
-const firstUser = usersArr[0]?.name ?? "Not Exist";
-const thirdUser = usersArr[2]?.name ?? "Not Exist";
+const firstUser = usersArr[0]?.name ?? 'Not Exist';
+const thirdUser = usersArr[2]?.name ?? 'Not Exist';
 
 console.log(firstUser);
 console.log(thirdUser);
 
 // With function
 const userObj = {
-  name: "Ahmed",
+  name: 'Ahmed',
   age: 30,
   setAddress() {
-    return "ismalia";
-  }
-}
+    return 'ismalia';
+  },
+};
 
-const addr = userObj.setAddress?.() ?? "No Address s";
-const addr2 = userObj.setLocation?.() ?? "No Location";
+const addr = userObj.setAddress?.() ?? 'No Address s';
+const addr2 = userObj.setLocation?.() ?? 'No Location';
 console.log(addr);
 console.log(addr2);
 
 // ------------------Objects loops-------------------------//
-console.log("---------Objects loops------------");
+console.log('---------Objects loops------------');
 
 const userObject = {
-  name: "Ahmed",
+  name: 'Ahmed',
   age: 30,
   address: {
-    country: "Egypt",
-    city: "ismailia",
-    street: "15B mazadat st"
-  }
-}
+    country: 'Egypt',
+    city: 'ismailia',
+    street: '15B mazadat st',
+  },
+};
 
 // 1) Print keys
 for (const key of Object.keys(userObject)) console.log(key);
@@ -279,12 +260,11 @@ for (const [key, value] of Object.entries(userObject)) {
   // live in ${country} in ${city} city in street ${street}`);
 }
 
-
 // ------------------Set-------------------------//
-console.log("---------Sets------------");
+console.log('---------Sets------------');
 
 // Goal : Remove duplicate values in array
-const stuff = ["waiter", "waiter", "manager", "chef", "chef"];
+const stuff = ['waiter', 'waiter', 'manager', 'chef', 'chef'];
 const positions = new Set(stuff);
 console.log(positions);
 
@@ -292,36 +272,67 @@ console.log(positions);
 console.log([...positions]);
 
 // Add elements & Remove elements
-positions.add("HR");
-positions.delete("manager");
+positions.add('HR');
+positions.delete('manager');
 
 // has element
-console.log(positions.has("HR"));
+console.log(positions.has('HR'));
 
 // Looping at sets
 for (const p of positions) console.log(p);
 
 // ------------------Maps-------------------------//
-console.log("---------Maps------------");
+console.log('---------Maps------------');
 // Goal: Maps like object but the big difference is map can contain different keys type
-const stringKey = "name";
+const stringKey = 'name';
 const numberKey = 12;
 const boolKey = true;
 
 const rest = new Map();
 
-rest.set(stringKey, "Ahmed");
+rest.set(stringKey, 'Ahmed');
 rest.set(numberKey, {
-  name: "ahmed",
-  age: 20
+  name: 'ahmed',
+  age: 20,
 });
-rest.set(boolKey, ["a", "b", "c"]);
+rest.set(boolKey, ['a', 'b', 'c']);
 
 // console.log(rest);
 // console.log(rest.get(stringKey));
 // console.log(rest.get(numberKey));
 // console.log(rest.get(boolKey));
 
-for (const item of rest) console.log(item)
-for (const item of rest.values()) console.log(item)
-for (const item of rest.keys()) console.log(item)
+for (const item of rest) console.log(item);
+for (const item of rest.values()) console.log(item);
+for (const item of rest.keys()) console.log(item);
+
+// ------------------String-------------------------//
+console.log('---------Strings------------');
+const companyName = 'Index Software Company Company';
+
+// indexOf() lastIndexOf()
+console.log(companyName.indexOf('o'));
+console.log(companyName.lastIndexOf('o'));
+
+// slice() ==> get last word in the statement
+console.log(companyName.slice(companyName.lastIndexOf(' ')));
+
+console.log(companyName.slice(companyName.indexOf(' ')));
+
+// trim()
+console.log('   hi ahmed boghdady    \n'.trim());
+
+// toLowerCase() toUpperCase()
+console.log(companyName.toLowerCase());
+console.log(companyName.toUpperCase());
+
+// replace() replaceAll()
+console.log(companyName.replace('Software', 'Group'));
+console.log(companyName.replaceAll('Company', 'Group'));
+
+// includes()
+console.log(companyName.includes('Index'));
+
+// startWith endsWith()
+console.log(companyName.startsWith('Index'));
+console.log(companyName.endsWith('Company'));
