4d352355db2d27bc02e1d8309762900a32902632
boghdady <boghdady107@gmail.com>
2020-12-22 23:06:18 +0200

    Passing value vs Refrences args

--- 06-functions/script.js
+++ 06-functions/script.js
@@ -15,3 +15,19 @@ const trip = function (flightNum, passengerNum = 1, price = 50 * passengerNum) {
 
 trip('1AB');
 trip('1B', undefined, 100);
+
+// Passing a premetive value vs Refrence value
+const flightNumber = '15B';
+const passenger = {
+  name: 'Ahmed',
+  password: 9838278,
+};
+
+const checkIn = function (flightNum, passengerData) {
+  flightNum = '15C';
+  passengerData.name = 'MS' + passengerData.name;
+};
+
+// Object value changed
+checkIn(flightNumber, passenger);
+console.log(flightNumber, passenger);
